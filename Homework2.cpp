637ee285a9d2d0dd88e0b51bad1d455ce995573f
IZenerO <romikgerus14@gmail.com>
2019-07-28 18:48:11 +0300

    Deleted useless symbols in cout operator, now cout contais <120 symbols

--- Homework2.cpp
+++ Homework2.cpp
@@ -9,21 +9,21 @@ int main() {
     const uint32_t CONST_2 = 2;
     const uint32_t LOW_RANGE_VALUE = 2;
     const uint32_t HIGH_RANGE_VALUE = 255;
-    std::cout << "Please, enter first argument of the equation in range [ " << LOW_RANGE_VALUE << "..." << HIGH_RANGE_VALUE << "]" << std::endl;
+    std::cout << "Please, enter first argument in range " << LOW_RANGE_VALUE << "..." << HIGH_RANGE_VALUE << std::endl;
     std::cin >> first_arg;
 
     if (first_arg < LOW_RANGE_VALUE || first_arg > HIGH_RANGE_VALUE) {
       std::cout << "You entered incorrect first argument of the equation: " << std::endl;
       continue;
     } 
-    std::cout << "Please, enter second argument of the equation in range [ " << LOW_RANGE_VALUE << "..." << HIGH_RANGE_VALUE << "]" << std::endl;
+    std::cout << "Please, enter second argument in range " << LOW_RANGE_VALUE << "..." << HIGH_RANGE_VALUE << std::endl;
     std::cin >> second_arg;
     
     if (second_arg < LOW_RANGE_VALUE || second_arg > HIGH_RANGE_VALUE) {
       std::cout << "You entered incorrect second argument of the equation: " << std::endl;
       continue;
     } 
-    std::cout << "Please, enter third argument of the equation in range [ " << LOW_RANGE_VALUE << "..." << HIGH_RANGE_VALUE << "]" << std::endl;
+    std::cout << "Please, enter third argument in range " << LOW_RANGE_VALUE << "..." << HIGH_RANGE_VALUE << std::endl;
     std::cin >> third_arg;
     
     if (third_arg < LOW_RANGE_VALUE || third_arg > HIGH_RANGE_VALUE) {
